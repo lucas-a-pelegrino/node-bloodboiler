b656a1178bc7230910ccaeea1e9a9823e75ff3fe
Lucas A Pelegrino <lucas.assuncao.p@gmail.com>
2019-03-14 11:33:14 -0300

    Removed swagger for testing purposes, there is an issue between supertest and swagger.

--- src/config/express/index.js
+++ src/config/express/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const swagger = require('swagger-express');
-const createSwaggerMiddleware = require('swagger-express-middleware');
+// const swagger = require('swagger-express');
+// const createSwaggerMiddleware = require('swagger-express-middleware');
 const { version } = require('../config');
 const routes = require('../../routes');
 const database = require('../../lib/database/mongodb');
@@ -10,35 +10,43 @@ const app = express();
 database.connect();
 
 app.set('port', process.env.PORT || 3000);
-const host = process.env.HOST || 'localhost';
+// const host = process.env.HOST || 'localhost';
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+// app.use(
+//   swagger.init(app, {
+//     apiVersion: '1.0',
+//     swaggerVersion: '2.0',
+//     swaggerURL: '/swagger',
+//     swaggerYML: './public/swagger.yml',
+//     swaggerUI: './public/swagger/',
+//     basePath: host,
+//     apis: ['./public/swagger.yml'],
+//     middleware: () => {},
+//   }),
+// );
+
+// createSwaggerMiddleware('./public/swagger.yml', app, (error, middleware) => {
+//   app.enable('strict routing');
+//   app.use(middleware.metadata());
+//   app.use(middleware.parseRequest());
+//   app.use(middleware.CORS(), middleware.validateRequest());
+//   app.use(bodyParser.json());
+//   app.use(
+//     bodyParser.urlencoded({
+//       extended: false,
+//     }),
+//   );
+
+//   app.use(`/api/${version}`, routes);
+// });
+app.use(bodyParser.json());
 app.use(
-  swagger.init(app, {
-    apiVersion: '1.0',
-    swaggerVersion: '2.0',
-    swaggerURL: '/swagger',
-    swaggerYML: './public/swagger.yml',
-    swaggerUI: './public/swagger/',
-    basePath: host,
-    apis: ['./public/swagger.yml'],
-    middleware: () => {},
+  bodyParser.urlencoded({
+    extended: true,
   }),
 );
 
-createSwaggerMiddleware('./public/swagger.yml', app, (error, middleware) => {
-  app.enable('strict routing');
-  app.use(middleware.metadata());
-  app.use(middleware.parseRequest());
-  app.use(middleware.CORS(), middleware.validateRequest());
-  app.use(bodyParser.json());
-  app.use(
-    bodyParser.urlencoded({
-      extended: false,
-    }),
-  );
-
-  app.use(`/api/${version}`, routes);
-});
+app.use(`/api/${version}`, routes);
 
 module.exports = app;
