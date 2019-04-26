6804b4eefe4f5c2c3d5d8da1e40f043f4777e7ce
Lucas A Pelegrino <lucas.assuncao.p@gmail.com>
2019-04-26 13:18:55 -0300

    Minor refactoring to error handling.

--- src/lib/errors/AuthorizationError.js
+++ src/lib/errors/AuthorizationError.js
@@ -2,6 +2,6 @@ const ApplicationError = require('./ApplicationError');
 
 module.exports = class AuthorizationError extends ApplicationError {
   constructor(message, status) {
-    super(message || 'an-unknown-error-occurred', status);
+    super(message || 'unexpected-error', status);
   }
 };
--- src/services/user/create.js
+++ src/services/user/create.js
@@ -3,13 +3,14 @@ const {
   createUser,
 } = require('../../repositories');
 const { encryptor } = require('../../utils');
+const ErrorHandler = require('../../lib/errors');
 
 module.exports = {
   createUser: async (params) => {
     try {
       const user = await getUserBy({ email: params.email });
       if (user) {
-        throw new Error('email-already-in-use');
+        throw new ErrorHandler.ApplicationError('email-already-in-use', 403);
       }
 
       params.password = encryptor.hashPassword(params.password); // eslint-disable-line
