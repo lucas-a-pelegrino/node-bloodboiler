fd54a84e57b9ef73ddd258074f8ff85fa317729f
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-04-16 20:16:42 -0300

    feat(users): adding projection to omit password and __v from documents

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,5 +1,5 @@
 const { create } = require('./create.service');
-const { list } = require('./list.servce');
+const { list } = require('./list.service');
 const { get } = require('./get.service');
 
 module.exports = {
