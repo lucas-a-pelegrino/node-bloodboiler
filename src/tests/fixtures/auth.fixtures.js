de423f3a0035624220709247743c81b65fd16202
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-04-18 05:18:52 -0300

    feat(authentication): adding authentication integration tests

---
+++ src/tests/fixtures/auth.fixtures.js
@@ -0,0 +1,41 @@
+const moment = require('moment');
+
+const {
+  test: { resetTokenExpiresTime, resetTokenExpiresTimeFormat },
+} = require('../../config/env');
+
+const { usersService } = require('../../services');
+const { jwt } = require('../../utils');
+
+const getSampleUser = async (id) => usersService.get(id);
+
+const generateExpiredToken = async (id) => {
+  const payload = {
+    sub: id,
+    exp: moment()
+      .subtract(resetTokenExpiresTime, resetTokenExpiresTimeFormat)
+      .unix(),
+  };
+
+  const token = await jwt.issue(payload);
+  await usersService.update(id, { passwordResetToken: token });
+
+  return token;
+};
+
+const generateSampleToken = async (id) => {
+  const payload = {
+    sub: id,
+    exp: moment()
+      .add(resetTokenExpiresTime, resetTokenExpiresTimeFormat)
+      .unix(),
+  };
+
+  return jwt.issue(payload);
+};
+
+module.exports = {
+  getSampleUser,
+  generateExpiredToken,
+  generateSampleToken,
+};
