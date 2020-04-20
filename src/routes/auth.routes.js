79084ea3b6e0abc6f7d8042d993919efd10a67e0
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-04-20 00:40:28 -0300

    feat(data-validation): adding data validation for auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const { authController } = require('../controllers');
+const { validate } = require('../middlewares');
+const {
+  validationSchemas: { auth },
+} = require('../helpers');
 
-router.post('/register', authController.register);
-router.post('/signin', authController.signin);
-router.post('/forgot-password', authController.forgotPassword);
-router.post('/:token/reset-password', authController.resetPassword);
+router.post('/register', validate(auth.register), authController.register);
+router.post('/signin', validate(auth.signin), authController.signin);
+router.post('/forgot-password', validate(auth.forgotPassword), authController.forgotPassword);
+router.post('/:token/reset-password', validate(auth.resetPassword), authController.resetPassword);
 
 module.exports.auth = router;
