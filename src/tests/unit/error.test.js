d1fbbdc3e51ef04a6d47dd14ce935273d8aaf41f
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-04-20 20:57:12 -0300

    feat(tests): fixing typo on error tracker tests

--- src/tests/unit/error.test.js
+++ src/tests/unit/error.test.js
@@ -1,6 +1,6 @@
 const httpMocks = require('node-mocks-http');
 const { errorTracker } = require('../../middlewares');
-const { ApplicationError } = require('../../utils');
+const { ApplicationError, logger } = require('../../utils');
 
 describe('Error Middlewares', () => {
   describe('Error Tracker', () => {
@@ -15,9 +15,9 @@ describe('Error Middlewares', () => {
       expect(next).toHaveBeenCalledWith(error);
     });
 
-    test('Should parse an Error to ApplicationError, keeping code and message', () => {
+    test('Should parse an Error to ApplicationError, keeping status and message', () => {
       const error = new Error('Some Random Error');
-      error.status = 400;
+      error.statusCode = 400;
       const req = httpMocks.createRequest();
       const res = httpMocks.createResponse();
       const next = jest.fn();
@@ -25,6 +25,13 @@ describe('Error Middlewares', () => {
       errorTracker(error, req, res, next);
 
       expect(next).toHaveBeenCalledWith(expect.any(ApplicationError));
+      expect(next).toHaveBeenCalledWith(
+        expect.objectContaining({
+          status: error.statusCode,
+          message: error.message,
+          isOperational: false,
+        }),
+      );
     });
 
     test('Should parse an Error without message and status to ApplicationError with 500 - Internal Server Error', () => {
@@ -36,6 +43,13 @@ describe('Error Middlewares', () => {
       errorTracker(error, req, res, next);
 
       expect(next).toHaveBeenCalledWith(expect.any(ApplicationError));
+      expect(next).toHaveBeenCalledWith(
+        expect.objectContaining({
+          status: 500,
+          message: 'Internal Server Error',
+          isOperational: false,
+        }),
+      );
     });
   });
 });
