e32a810ea8c47b3d0260e6e474b5fcaa89486c5e
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-04-20 00:27:11 -0300

    feat(data-validation): adding rest of the validation helpers for users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,10 +7,10 @@ const {
 
 router.use(isAuthorized);
 
-router.get('/', validate(users.get), usersController.list);
-router.get('/:id', usersController.get);
-router.post('/', usersController.create);
-router.put('/:id', usersController.update);
-router.delete('/:id', usersController.destroy);
+router.get('/', validate(users.list), usersController.list);
+router.get('/:id', validate(users.get), usersController.get);
+router.post('/', validate(users.create), usersController.create);
+router.put('/:id', validate(users.update), usersController.update);
+router.delete('/:id', validate(users.destroy), usersController.destroy);
 
 module.exports.users = router;
