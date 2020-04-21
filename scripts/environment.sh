bccc6b387874af835e843d66e10ec3d6893625f4
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-04-21 11:37:44 -0300

    ci(build): creating script to set env vars on build

---
+++ scripts/environment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "NODE_ENV=development
+PORT=3000
+CLIENT_URL=http://localhost:3000
+
+JWT_SECRET=${{ secrets.JWT_SECRET }}
+
+RESET_TOKEN_EXPIRES_TIME=30
+RESET_TOKEN_EXPIRES_TIME_FORMAT=minutes
+
+DB_NAME=node-bloodboiler
+DB_HOST=localhost
+DB_PORT=27017
+DB_USER=
+DB_PASSWORD=
+
+MAIL_FROM=no-reply <apps@ioasys.com.br>
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=465
+MAIL_USER=apps@ioasys.com.br
+MAIL_PASS=${{ secrets.MAIL_PASS }}" > .env
