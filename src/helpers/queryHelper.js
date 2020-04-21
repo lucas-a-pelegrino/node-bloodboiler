d9c5c38ac53c3f0c4169a3abe98646d9aa881da8
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-04-21 17:02:35 -0300

    feat(query-builder): chaging location of the .trim method

--- src/helpers/queryHelper.js
+++ src/helpers/queryHelper.js
@@ -1,7 +1,7 @@
 const { ApplicationError } = require('../utils');
 
 module.exports.queryHelper = (options) => {
-  const sort = options.sortBy.trim() || 'createdAt:desc';
+  const sort = options.sortBy || 'createdAt:desc';
   const limit = parseInt(options.perPage || 10, 10);
   let skip = parseInt(options.page || 1, 10);
 
@@ -28,13 +28,13 @@ module.exports.queryHelper = (options) => {
     $skip: skip,
   });
 
-  const [sortKey, sortVvalue] = sort.split(':');
-  if (!['asc', 'desc'].includes(sortVvalue)) {
+  const [sortKey, sortValue] = sort.trim().split(':');
+  if (!['asc', 'desc'].includes(sortValue)) {
     throw new ApplicationError("Sort order must be one of the following: 'asc' or 'desc'", 400);
   }
 
   pipeline[0].$facet.data.push({
-    $sort: { [sortKey]: sortVvalue === 'desc' ? -1 : 1 },
+    $sort: { [sortKey]: sortValue === 'desc' ? -1 : 1 },
   });
 
   return pipeline;
