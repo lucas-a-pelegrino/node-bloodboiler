e69b48940e8a6464c0f7ec54cbc8ba22c016505f
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-04-27 20:21:40 -0300

    feat(query-builder): fix on paginating logic

--- src/services/users/list.service.js
+++ src/services/users/list.service.js
@@ -7,13 +7,14 @@ module.exports.list = async (options) => {
   query[0].$facet.data.push({ $project: { password: 0, __v: 0 } });
 
   const [{ metadata, data }] = await usersRepository.list(query);
+  const totalPages = Math.ceil(metadata.total / options.perPage);
 
   return {
     metadata: {
       ...metadata,
-      totalPages: Math.ceil(metadata.total / options.perPage),
+      totalPages,
       ...(options.page > 1 && { previousPage: options.page - 1 }),
-      ...(options.page < metadata.total && { nextPage: options.page + 1 }),
+      ...(options.page < metadata.total && options.page < totalPages && { nextPage: options.page + 1 }),
     },
     data,
   };
