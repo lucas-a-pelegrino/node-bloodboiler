32ed12283098d071a571d8d08f247f97a373fb78
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-12-24 01:59:59 -0300

    feat(access-tokens): added create tokens service

---
+++ src/services/accessTokens/create.service.js
@@ -0,0 +1,43 @@
+const moment = require('moment');
+
+const { accessTokenExpiresIn, refreshTokenExpiresIn } = require('../../config/env');
+const { encryptor } = require('../../helpers');
+const { usersRepository } = require('../../repositories');
+
+module.exports = {
+  create: async (payload) => {
+    const accessToken = encryptor.generateToken(
+      {
+        ...payload,
+        iat: moment().unix(),
+      },
+      {
+        algorithm: 'HS384',
+        expiresIn: accessTokenExpiresIn,
+      },
+    );
+
+    const refreshToken = encryptor.generateToken(
+      {
+        sub: {
+          id: payload.sub.id,
+        },
+        iat: moment().unix(),
+      },
+      {
+        algorithm: 'HS256',
+        expiresIn: refreshTokenExpiresIn,
+      },
+    );
+
+    const user = usersRepository.getById(payload.sub.id);
+    user.tokens.push({ token: accessToken, type: 'access' }, { token: refreshToken, type: 'refresh' });
+
+    await usersRepository.update(user);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  },
+};
