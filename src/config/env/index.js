33b3da7d279ff2fb09b64e379ee84195b4a81560
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-12-24 20:40:16 -0300

    build: added expiration time vars on env config

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -9,6 +9,8 @@ module.exports = {
   clientURL: process.env.CLIENT_URL,
   version: 'v1',
   secret: process.env.JWT_SECRET,
+  accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+  refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
   resetTokenExpiresTime: process.env.RESET_TOKEN_EXPIRES_TIME,
   resetTokenExpiresTimeFormat: process.env.RESET_TOKEN_EXPIRES_TIME_FORMAT,
   corsOptions: {
