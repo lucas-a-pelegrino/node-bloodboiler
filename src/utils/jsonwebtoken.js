076fab43c2e8bb510a56e17567094ac821ac373e
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2021-02-02 17:41:51 -0300

    refactor(auth): refactoring jwt.verify to handle erros thrown by the external lib

--- src/utils/jsonwebtoken.js
+++ src/utils/jsonwebtoken.js
@@ -1,8 +1,18 @@
 const jwt = require('jsonwebtoken');
+const { StatusCodes } = require('http-status-codes');
 
 const { secret } = require('../config/env');
+const { ApplicationError } = require('./ApplicationError');
 
 module.exports.jwt = {
-  issue: (payload) => jwt.sign(payload, secret, { algorithm: 'HS256' }),
-  verify: (token, callback) => jwt.verify(token, secret, callback),
+  issue: (payload, options) => jwt.sign(payload, secret, options),
+  verify: (token) =>
+    jwt.verify(token, secret, { algorithms: ['HS384', 'HS256'] }, (err, decoded) => {
+      if (err) {
+        const message = err.message.replace(/\s/g, '-');
+        throw new ApplicationError(message, StatusCodes.UNAUTHORIZED);
+      }
+
+      return decoded;
+    }),
 };
