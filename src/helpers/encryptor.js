f23a966c3285102fd19006db4e03a2193ca6d480
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2021-02-03 00:09:27 -0300

    refactor(jwt): renaming jwt methods, handling token expired error from lib
    Renaming jsonwebtoken.js methods from issue/verify to signToken/verifyToken;
    Adding ExpiredTokenError handler on verifyToken method;

--- src/helpers/encryptor.js
+++ src/helpers/encryptor.js
@@ -4,5 +4,5 @@ const { jwt } = require('../utils');
 module.exports.encryptor = {
   hashPassword: (password) => bcrypt.hash(password, 8),
   comparePassword: (password, userPassword) => bcrypt.compare(password, userPassword),
-  generateToken: (payload, options) => jwt.issue(payload, options),
+  generateToken: (payload, options) => jwt.signToken(payload, options),
 };
--- src/utils/jsonwebtoken.js
+++ src/utils/jsonwebtoken.js
@@ -2,17 +2,24 @@ const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
 
 const { secret } = require('../config/env');
+const { messages } = require('../helpers/messages');
 const { ApplicationError } = require('./ApplicationError');
 
-module.exports.jwt = {
-  issue: (payload, options) => jwt.sign(payload, secret, options),
-  verify: (token) =>
-    jwt.verify(token, secret, { algorithms: ['HS384', 'HS256'] }, (err, decoded) => {
+module.exports = {
+  signToken: (payload, options) => jwt.sign(payload, secret, options),
+  verifyToken: (token) => {
+    return jwt.verify(token, secret, { algorithms: ['HS384', 'HS256'] }, (err, decoded) => {
       if (err) {
         const message = err.message.replace(/\s/g, '-');
-        throw new ApplicationError(message, StatusCodes.UNAUTHORIZED);
+
+        if (err.name === 'TokenExpiredError') {
+          throw new ApplicationError(messages.expiredToken, StatusCodes.FORBIDDEN);
+        } else {
+          throw new ApplicationError(message, StatusCodes.UNAUTHORIZED);
+        }
       }
 
       return decoded;
-    }),
+    });
+  },
 };
