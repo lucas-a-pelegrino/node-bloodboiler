8f10a94d8361d2b3d996f3ecb0fa3a65a19937fd
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2021-02-13 02:29:43 -0300

    refactor(middlewares): refactoring isAuthorized to match new token rules format

--- src/middlewares/isAuthorized.js
+++ src/middlewares/isAuthorized.js
@@ -22,19 +22,9 @@ module.exports = catchAsync(async (req, res, next) => {
     sub: { id: userId },
   } = await jwt.verifyToken(token);
 
-  const decodedUser = await usersService.get(userId);
+  const user = await usersService.get(userId);
 
-  const userAccessToken = decodedUser.tokens.find((AccessToken) => AccessToken.access === token);
-
-  if (!userAccessToken) {
-    throw new ApplicationError(messages.notFound('token'), StatusCodes.NOT_FOUND);
-  }
-
-  if (userAccessToken.expired) {
-    throw new ApplicationError(messages.expiredToken, StatusCodes.FORBIDDEN);
-  }
-
-  req.session = { token, _id: decodedUser._id, email: decodedUser.email };
+  req.session = { token, _id: user._id, email: user.email };
 
   next();
 });
